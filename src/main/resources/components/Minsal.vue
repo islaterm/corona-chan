6fa7e45f923ff917381b4a43cc8cd858fbcb1690
Roäc <smflako@gmail.com>
2020-05-20 21:52:19 -0400

    Hotfix
    Signed-off-by: Roäc <smflako@gmail.com>

--- src/main/resources/components/Minsal.vue
+++ src/main/resources/components/Minsal.vue
@@ -1,21 +1,3 @@
-<template>
-    <v-card class="d-inline-block mb-2 mx-xs-0 mx-2" elevation="0">
-        <v-col id="DATOS_MINSAL" class="mb-5" cols="12" justify="center">
-            <h2 class="headline font-weight-bold mb-3">
-                Información oficial del Ministerio de Salud
-            </h2>
-            <v-row class="mb-5" align="center" justify="center">
-                <Infections />
-                <Quarantines />
-            </v-row>
-
-            <v-row id="FOOTNOTE" justify="center">
-                <span class="mx-2" v-html="footnote.html"></span>
-            </v-row>
-        </v-col>
-    </v-card>
-</template>
-
 <script>
     import Infections from "./minsal/Infections"
     import Quarantines from "./minsal/Quarantines"
